d268b42c928ff9bbdc37fc67e9ba8219685b35bd
Paulo Soares <62898736+opaulosoares@users.noreply.github.com>
2021-12-13 17:52:59 -0300

    Major changes to UI part 3

--- lib/backend/enemy.h
+++ lib/backend/enemy.h
@@ -3,52 +3,49 @@
 
 #include "./lib/backend/rng.h"
 #include <string>
-#include <mutex>
 
 #define BASE_ENEMY_HP 100
 #define BASE_COIN_DROP 15
 #define BASE_EXP_DROP 30
 
-std::string adjectiveList[] = {
-	"Mighty",
-	"Fearsome",
-	"Angry",
-	"Robust",
-	"Indomitable",
+const std::string adjectiveList[] = {
+    "Mighty",
+    "Fearsome",
+    "Angry",
+    "Robust",
+    "Indomitable",
 };
 
-std::string specimenList[] = {
+const std::string specimenList[] = {
     "Mage",
-	"Wolf",
+    "Wolf",
     "Sorcerer",
     "Slayer",
-	"Elf",
+    "Elf",
 };
 
 class Enemy : private Rng {
 private:
-	int enemyFloor;
+    int enemyFloor;
     std::string enemyAdjective;
     std::string enemySpecimen;
-	int currHP;
-	int maxHP;
-
-	std::mutex hpMutex;
+    int currHP;
+    int maxHP;
 
     std::string getRandomAdjective();
     std::string getRandomSpecimen();
-	int generateNewEnemy(int currFloor);
+    int generateNewEnemy(int currFloor);
 
 public:
-	Enemy(int currFloor=1);
+    Enemy(int currFloor = 1);
 
-	bool inflictDamage(int damageTaken);
-	int getEnemyFloor();
+    bool inflictDamage(int damageTaken);
+    int getEnemyFloor();
     std::string getMobName();
-	int getCurrHP();
-	int getMaxHP();
-	int dropCoinsOnDeath();
-	int dropExpOnDeath();
+    int getCurrHP();
+    int getMaxHP();
+    int dropCoinsOnDeath();
+    int dropExpOnDeath();
 };
 
 #endif
