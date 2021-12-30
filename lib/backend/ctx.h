b7c5793b3f5342b0e0de738fb71cdd47cef06eac
natan-dot-com <natanhsanches@gmail.com>
2021-12-29 22:02:37 -0300

    Fixed skills thread interation
    Refactored most part of the main context code

--- lib/backend/ctx.h
+++ lib/backend/ctx.h
@@ -8,9 +8,51 @@
 #include <chrono>
 #include <atomic>
 
-typedef struct _threadInstance {
+#include <iostream>
+
+#define CD_CLICK_MILIS 500
+#define CD_FIREBALL_SECS 10
+#define CD_DESTAURA_SECS 15
+#define DELAY_DESTAURA_DOT_SECS 1
+
+#define DESTAURA_DOT_TICKS 6
+
+typedef struct _TurnResults {
+	int damageDealt;
+	int gainedExp;
+	int gainedCoins;
+	bool isLevelUp;
+
+	_TurnResults(int _damageDealt, int _gainedExp, int _gainedCoins, bool _isLevelUp) {
+		this->damageDealt = _damageDealt;
+		this->gainedExp = _gainedExp;
+		this->gainedCoins = _gainedCoins;
+		this->isLevelUp = _isLevelUp;
+	}
+
+	void printResults() {
+		std::cout << "Damage: " << this->damageDealt << '\n'
+  		   		  << "Exp: " << this->gainedExp << '\n'
+		 		  << "Coins: " << this->gainedCoins << '\n'
+		 		  << "Level Up: " << this->isLevelUp << '\n';
+	}
+} TurnResults;
+
+typedef struct _ThreadInstance {
 	std::thread currThread;
 	std::atomic<bool> executionFlag;
+
+	_ThreadInstance() {
+		this->executionFlag = false;
+	}
+
+	void toggleUsage() {
+		this->executionFlag = !this->executionFlag;
+	}
+
+	bool isInUse() {
+		return this->executionFlag;
+	}
 } ThreadInstance;
 
 class Context {
@@ -18,12 +60,13 @@ private:
 	ThreadInstance *fireballExec;
 	ThreadInstance *clickExec;
 	ThreadInstance *destAuraExec;
+	ThreadInstance *destAuraDmg;
 
 	int currFloor;
 
-	void damageFireball();
-	void damageDestructionAura();
-	void damageOnClick();
+	void startCooldown(ThreadInstance *cooldownThread, int timeAmount, const int MODE);
+	bool proccessMonsterDamage(const int dealtDamage, int &gainedExp, int &gainedCoins, bool &isLevelUp);
+	void damageDestructionAura(const int dealtDamage, int &gainedExp, int &gainedCoins, bool &isLevelUp);
 
 public:
 	Liesel *playerInstance;
@@ -37,9 +80,9 @@ public:
 	bool saveGame(std::string targetFilename);
 
 	// Damage/skills related methods
-	void evokeDamageOnClick();
-	void evokeFireball();
-	void evokeDestructionAura();
+	TurnResults *evokeDamageOnClick();
+	TurnResults *evokeFireball();
+	TurnResults *evokeDestructionAura();
 
 	// Skills management
 	bool updateFireball();
--- src/backend/ctx.cpp
+++ src/backend/ctx.cpp
@@ -1,169 +1,208 @@
 #include "./lib/backend/ctx.h"
 
-#include <iostream>
+#define MODE_SECS -10
+#define MODE_MILIS -20
 
-#define DOT_TICKS 6
-
+// Main constructor for context class
 Context::Context() {
 	currFloor = 1;
 
 	playerInstance =  new Liesel();
 	currEnemyInstance =  new Enemy(currFloor);
-	errHandler =  new Error();
+	errHandler = new Error();
+
 	fireballExec = new ThreadInstance();
 	clickExec = new ThreadInstance();
 	destAuraExec = new ThreadInstance();
-
-	fireballExec->executionFlag = false;
-	clickExec->executionFlag = false;
-	destAuraExec->executionFlag = false;
+	destAuraDmg = new ThreadInstance();
 }
 
-// Thread dependant: Deals damage to enemy based on player's level
-void Context::damageOnClick() {
-	int dealtDamage = playerInstance->generateDamageOnClick();
+// Start the current skill cooldown on its representative thread
+// - Toggles the usage indicator of the predetermined thread (as 'cooldownThread')
+// - Sleeps thread for 'timeAmount' seconds/milisseconds, depending of the used 'MODE'.
+void Context::startCooldown(ThreadInstance *cooldownThread, const int timeAmount, const int MODE) {
+	if (timeAmount > 0) {
+		cooldownThread->toggleUsage();
+		switch (MODE) {
+			case MODE_SECS:
+				std::this_thread::sleep_for(std::chrono::seconds(timeAmount));
+				break;
 
-	// Process rewards and a new enemy if the current one dies
-	if (currEnemyInstance->inflictDamage(dealtDamage)) {
-		int gainedExp = currEnemyInstance->dropExpOnDeath();
-		int gainedCoins = currEnemyInstance->dropCoinsOnDeath();
-
-		Enemy *newEnemy = new Enemy(currFloor);
-		delete currEnemyInstance;
-		currEnemyInstance = newEnemy;
-
-		playerInstance->updateGainedCoins(gainedCoins);
-        playerInstance->updateExp(gainedExp);
+			case MODE_MILIS:
+				std::this_thread::sleep_for(std::chrono::milliseconds(timeAmount));
+				break;
+		}
+		cooldownThread->toggleUsage();
 	}
-	// 0.5s delay for each damage dealt
-	std::this_thread::sleep_for(std::chrono::milliseconds(500));
-	this->clickExec->executionFlag = false;
 }
 
-// Calls click damage thread
-void Context::evokeDamageOnClick() {
-	if (!this->clickExec->executionFlag) {
+// Proccess and refresh the monster HP bar after an attack.
+// - Also immediately evoke a new enemy if the current one is dead after the attack.
+// - Returns bool indicating whether the current enemy is dead ('true') or not ('false').
+bool Context::proccessMonsterDamage(const int dealtDamage, int &gainedExp, int &gainedCoins, bool &isLevelUp) {
+	if (currEnemyInstance->inflictDamage(dealtDamage)) {
+		gainedExp = this->currEnemyInstance->dropExpOnDeath();
+		gainedCoins = this->currEnemyInstance->dropCoinsOnDeath();
+
+		// Spawn new enemy
+		Enemy *newEnemy = new Enemy(currFloor);
+		delete this->currEnemyInstance;
+		this->currEnemyInstance = newEnemy;
+
+		this->playerInstance->updateGainedCoins(gainedCoins);
+        isLevelUp = this->playerInstance->updateExp(gainedExp);
+        return true;
+	}
+	return false;
+}
+
+// Proccess damage on click.
+// - Cooldown between clicks: 0.5s (500ms) on 'clickExec' thread.
+// - Returns all the details from the current action, such as the damage dealt,
+// 	 the experience gained, if the player leveled up.
+TurnResults *Context::evokeDamageOnClick() {
+	if (!this->clickExec->isInUse()) {
+
+		// Reset the functionality of previously used thread
 		if (this->clickExec->currThread.joinable()) {
 			this->clickExec->currThread.join();
 		}
-		this->clickExec->executionFlag = true;
-		this->clickExec->currThread = std::thread(&Context::damageOnClick, this);
+
+		// Proccess click damage system
+		const int dealtDamage = this->playerInstance->generateDamageOnClick();
+		int gainedExp = 0;
+		int gainedCoins = 0;
+		bool isLevelUp = false;
+
+		// Proccess rewards and a new enemy if the current one dies
+		this->proccessMonsterDamage(dealtDamage, gainedExp, gainedCoins, isLevelUp);
+
+		// Cooldown: 0.5s delay for each damage dealt
+		this->clickExec->currThread = std::thread(&Context::startCooldown, this, this->clickExec, CD_CLICK_MILIS, MODE_MILIS);
+
+		return new TurnResults(dealtDamage, gainedExp, gainedCoins, isLevelUp);
 	}
+	return NULL;
 }
 
-// Thread dependant: Casts an fireball at the enemy. Damage's dealt based on the current skill level
-void Context::damageFireball() {
-	int dealtDamage = playerInstance->fireSkill.skillEffect();
-	if (dealtDamage != 0) {
+// Casts a fireball, dealing extra damage.
+// - Cooldown between fireball casts: 10s on 'fireballExec' thread.
+// - Returns all the details from the current action, such as the damage dealt,
+// 	 the experience gained, if the player leveled up.
+TurnResults *Context::evokeFireball() {
+	if (!this->fireballExec->isInUse()) {
 
-		// Process rewards and a new enemy if the current one dies
-		if (currEnemyInstance->inflictDamage(dealtDamage)) {
-			int gainedExp = currEnemyInstance->dropExpOnDeath();
-			int gainedCoins = currEnemyInstance->dropCoinsOnDeath();
-
-			Enemy *newEnemy = new Enemy(currFloor);
-			delete currEnemyInstance;
-			currEnemyInstance = newEnemy;
-
-			playerInstance->updateGainedCoins(gainedCoins);
-            playerInstance->updateExp(gainedExp);
-		}
-
-		// 10s cooldown for each cast
-		std::this_thread::sleep_for(std::chrono::seconds(10));
-	}
-	this->fireballExec->executionFlag = false;
-}
-
-// Calls fireball thread
-void Context::evokeFireball() {
-	if (!this->fireballExec->executionFlag) {
+		// Reset the functionality of previously used thread
 		if (this->fireballExec->currThread.joinable()) {
 			this->fireballExec->currThread.join();
 		}
-		if (playerInstance->fireSkill.getLevel() > 0) {
-			this->fireballExec->executionFlag = true;
-			this->fireballExec->currThread = std::thread(&Context::damageFireball, this);
-		}
+
+		// Proccess fireball damage system
+		const int dealtDamage = this->playerInstance->fireSkill.skillEffect();
+		int gainedExp = 0;
+		int gainedCoins = 0;
+		bool isLevelUp = false;
+
+		// Proccess rewards and a new enemy if the current one dies
+		this->proccessMonsterDamage(dealtDamage, gainedExp, gainedCoins, isLevelUp);
+
+		// Cooldown: 10s
+		this->fireballExec->currThread = std::thread(&Context::startCooldown, this, this->fireballExec, CD_FIREBALL_SECS, MODE_SECS);
+
+		return new TurnResults(dealtDamage, gainedExp, gainedCoins, isLevelUp);
 	}
+	return NULL;
 }
 
-
-// Thread dependant: Casts a DoT (Damage Over Time) aura which deals damage based on skill's current level 
-void Context::damageDestructionAura() {
+// Auxiliary function which proccess the total damage dealt in 'destAuraDmg' thread.
+// PROBLEM: referencing variables inside a thread call (think how to fix it)
+void Context::damageDestructionAura(const int dealtDamage, int &gainedExp, int &gainedCoins, bool &isLevelUp) {
 	int damageCounter = 0;
 
-	// Each 2 seconds, a damage tick occurs. Max damage goes around DOT_TICKS ticks.
-	while(damageCounter < DOT_TICKS) {
+	this->destAuraDmg->toggleUsage();
+
+	// Each second, a damage tick occurs. Max damage goes around 'DESTAURA_DOT_TICKS' ticks.
+	while(damageCounter < DESTAURA_DOT_TICKS) {
 
 		// Process rewards and a new enemy if the current one dies
-		if (currEnemyInstance->inflictDamage(playerInstance->destructionSkill.getCurrDoT())) {
-			int gainedExp = currEnemyInstance->dropExpOnDeath();
-			int gainedCoins = currEnemyInstance->dropCoinsOnDeath();
-
-			Enemy *newEnemy = new Enemy(currFloor);
-			delete currEnemyInstance;
-			currEnemyInstance = newEnemy;
-
-			playerInstance->updateGainedCoins(gainedCoins);
-            playerInstance->updateExp(gainedExp);
-		}
+		this->proccessMonsterDamage(dealtDamage, gainedExp, gainedCoins, isLevelUp);
 		damageCounter++;
-		// Delay between ticks (2 seconds)
-		std::this_thread::sleep_for(std::chrono::seconds(2));
+
+		// Delay between ticks (1 second)
+		std::this_thread::sleep_for(std::chrono::seconds(DELAY_DESTAURA_DOT_SECS));
 	}
-	// 15s cooldown for each cast
-	std::this_thread::sleep_for(std::chrono::seconds(15));
-	this->destAuraExec->executionFlag = false;
+	this->destAuraDmg->toggleUsage();
 }
 
-// Calls destruction aura thread
-void Context::evokeDestructionAura() {
-	if (!this->destAuraExec->executionFlag) {
+// Casts destruction aura, dealing its equivalente DoT damage over 'DESTAURA_DOT_TICKS' ticks.
+// - Cooldown between destruction aura casts: 15s on 'destAuraExec' thread.
+// - Cooldown between destruction aura damage ticks: 1s on 'destAuraDmg' thread.
+// - Returns all the details from the current action, such as the damage dealt,
+// 	 the experience gained, if the player leveled up.
+TurnResults *Context::evokeDestructionAura() {
+	if (!this->destAuraExec->isInUse()) {
+
+		// Proccess destruction aura DoT-based damage system
+		const int dealtDamage = this->playerInstance->destructionSkill.getCurrDoT();
+		int gainedExp = 0;
+		int gainedCoins = 0;
+		bool isLevelUp = false;
+
+		// Reset the functionality of previously used thread (destruction aura damage thread)
+		if (this->destAuraDmg->currThread.joinable()) {
+			this->destAuraDmg->currThread.join();
+		}
+		if (!this->destAuraDmg->isInUse()) {
+			this->destAuraDmg->currThread = std::thread(&Context::damageDestructionAura, this, 
+					dealtDamage, std::ref(gainedExp), std::ref(gainedCoins), std::ref(isLevelUp));	
+		}
+
+		// Reset the functionality of previously used thread (destruction aura cooldown thread)
 		if (this->destAuraExec->currThread.joinable()) {
 			this->destAuraExec->currThread.join();
 		}
-		if (playerInstance->destructionSkill.getLevel() > 0) {
-			this->destAuraExec->executionFlag = true;
-			this->destAuraExec->currThread = std::thread(&Context::damageDestructionAura, this);
-		}
+		// Cooldown: 15s
+		this->destAuraExec->currThread = std::thread(&Context::startCooldown, this, this->destAuraExec, CD_FIREBALL_SECS, MODE_SECS);
+
+		return new TurnResults(dealtDamage, gainedExp, gainedCoins, isLevelUp);
 	}
+	return NULL;
 }
 
 // Upgrade skills methods
 bool Context::updateFireball() {
-	int currSoulCoins = playerInstance->getSoulCoins();
-	playerInstance->updateSpentCoins(currSoulCoins);
-	return playerInstance->fireSkill.updateExp(currSoulCoins);
+	const int currSoulCoins = this->playerInstance->getSoulCoins();
+	this->playerInstance->updateSpentCoins(currSoulCoins);
+	return this->playerInstance->fireSkill.updateExp(currSoulCoins);
 }
 
 bool Context::updateChronomancy() {
-	int currSoulCoins = playerInstance->getSoulCoins();
-	playerInstance->updateSpentCoins(currSoulCoins);
-	return playerInstance->chronoSkill.updateExp(currSoulCoins);
+	const int currSoulCoins = this->playerInstance->getSoulCoins();
+	this->playerInstance->updateSpentCoins(currSoulCoins);
+	return this->playerInstance->chronoSkill.updateExp(currSoulCoins);
 }
 
 bool Context::updateDestructionAura() {
-	int currSoulCoins = playerInstance->getSoulCoins();
-	playerInstance->updateSpentCoins(currSoulCoins);
-	return playerInstance->destructionSkill.updateExp(currSoulCoins);
+	const int currSoulCoins = this->playerInstance->getSoulCoins();
+	this->playerInstance->updateSpentCoins(currSoulCoins);
+	return this->playerInstance->destructionSkill.updateExp(currSoulCoins);
 }
 
 // Current floor management methods
 void Context::nextFloor() {
-	currFloor++;
-	delete currEnemyInstance;
-	currEnemyInstance = new Enemy(currFloor);
+	this->currFloor++;
+	delete this->currEnemyInstance;
+	this->currEnemyInstance = new Enemy(currFloor);
 }
 
 void Context::previousFloor() {
-	if (currFloor > 1) {
-		currFloor--;
-		delete currEnemyInstance;
-		currEnemyInstance = new Enemy(currFloor);
+	if (this->currFloor > 1) {
+		this->currFloor--;
+		delete this->currEnemyInstance;
+		this->currEnemyInstance = new Enemy(currFloor);
 	}
 }
 
 int Context::getCurrentFloor() {
-	return currFloor;
+	return this->currFloor;
 }
